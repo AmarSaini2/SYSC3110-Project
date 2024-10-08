69a2f8779ce62e01c86e1d2f5ae91e94b2286a2a
AmarSaini2 <amardsaini2@gmail.com>
2024-10-08 12:34:34 -0400

    First push

---
+++ src/tile.java
@@ -0,0 +1,44 @@
+import java.util.Formatter;
+
+public class tile {
+    private String letter;
+    private int points;
+
+    tile(String letter){
+        this.letter = letter;
+        switch(letter){
+            case("a"), ("e"), ("i"), ("l"), ("n"), ("o"), ("r"), ("s"), ("t"), ("u"):
+                this.points = 1;
+                break;
+            case("b"), ("c"), ("m"), ("p"):
+                this.points = 3;
+                break;
+            case("d"), ("g"):
+                this.points = 2;
+                break;
+            case("f"), ("h"), ("v"), ("w"), ("y"):
+                this.points = 4;
+                break;
+            case("j"), ("x"):
+                this.points = 8;
+                break;
+            case("k"):
+                this.points = 5;
+                break;
+            case("q"), ("z"):
+                this.points = 10;
+                break;
+            case("blank"):
+                this.points = 0;
+                break;
+        }
+    }
+
+    public String getID(){
+        return this.letter;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
+}
---
+++ src/wordbag.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class wordbag {
+    private ArrayList<tile> tiles;
+
+    wordbag(){
+        tiles = new ArrayList<tile>(){"blank","blank","a","a","a","a","a","a","a","a","a","b","b","c","c","d","d","d","d","e","e","e","e","e","e","e","e","e","e","e","e", "f","f","g","g","g","h","h","i","i","i","i","i","i","i","i","i","j","k","l","l","l","l","m","m","n","n","n","n","n","n","o","o","o","o","o","o","o","o","p","p","q","r","r","r","r","r","r","s","s","s","s","t","t","t","t","t","t","u","u","u","u","v","v","w","w","x","y","y","z"};
+    }
+
+    public String listTiles(){
+        String returnString = "Available Letters:";
+
+    }
+
+}
