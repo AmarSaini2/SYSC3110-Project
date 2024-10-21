10a17ec1d935301ad02fba5324e747db2deb536f
chad-garfield <bacongoalie33@gmail.com>
2024-10-20 20:22:32 -0400

    changes made to game and player to fix bugs: weird output after returning invalid taking out tiles when player plays an invalid word proper turn order

--- src/Game.java
+++ src/Game.java
@@ -26,6 +26,7 @@ public class Game {
         String p2Name = getPlayerName();
         Game game = new Game(p1Name, p2Name);
         System.out.println("Welcome " + game.p1.getName() + " and " + game.p2.getName());
+        turnLoop:
         while(!game.p1.handIsEmpty() && !game.p2.handIsEmpty()){//if either player still has tiles, keep playing
             while(!game.playerTurn(game.p1)){// until player plays a valid turn, loop back to player
                 game.playerTurn(game.p1);
@@ -59,7 +60,7 @@ public class Game {
     public boolean playerTurn(Player currentPlayer){
         //player turn order: pick horizontal or vertical, place tiles until either submit is entered or hand is empty, submit turn for review -> GOTO submit();
         //A temp copy of the board is made to display as the player places tiles before submitting. A temp copy of the hand will be used for the same purpose
-        ArrayList<Tile> tempHand = currentPlayer.getHand();
+        ArrayList<Tile> tempHand = new ArrayList<Tile>(currentPlayer.getHand());
         Board tempBoard = new Board(board);
         int firstRowOrCol;//tracker to lock player to row/column
         tempBoard.display();
@@ -67,39 +68,40 @@ public class Game {
 
         currentPlayer.printHand();
 
-        System.out.println("Choose a direction to place your word in (h/v):");
         String direction;
         while(true){//get placement direction
+            System.out.println("Choose a direction to place your word in (h/v):");
             try{
                 direction = in.next();
                 if(!(direction.equalsIgnoreCase("h") || direction.equalsIgnoreCase("v"))){
                     System.out.println("Invalid input");
-                    in.next();
+                    continue;
                 }else{
                     break;
                 }
             }catch(InputMismatchException e){
                 System.out.println("Invalid input");
-                in.next();
+                continue;
             }
         }
 
-        if(direction.equals("h")){
-            System.out.println("Enter the row you would like to place letters across");
-        }else{
-            System.out.println("Enter the column you would like to place letters down");
-        }
-
         while(true){//get first row or column (depending on direction selection)
+            if(direction.equals("h")){
+                System.out.println("Enter the row you would like to place letters across");
+            }else{
+                System.out.println("Enter the column you would like to place letters down");
+            }
             try{
                 firstRowOrCol = in.nextInt();
                 if(firstRowOrCol < 0 || firstRowOrCol > 15){
-                    System.out.println("Invalid input");
+                    System.out.println("Invalid input: must be between 0 and 15");
+                    continue;
                 }else{
                     break;
                 }
             }catch(InputMismatchException e){
                 System.out.println("Invalid input");
+                continue;
             }
         }
 
@@ -130,7 +132,7 @@ public class Game {
                     }
                 } catch (InputMismatchException e) {
                     System.out.println("Invalid input.");
-                    in.next();
+                    continue;
                 }
             }
 
@@ -140,6 +142,7 @@ public class Game {
                 System.out.println("Please input a row to place " + tileName + " in");
             }
 
+            //program loop for entering a letter
             int row, col;
             while (true) {
                 try {
@@ -167,15 +170,17 @@ public class Game {
                         break;
                     } else {
                         System.out.println("Cannot place on an occupied spot");
+                        tempBoard.display();
                     }
                 } catch (InputMismatchException e) {
                     System.out.println("Invalid input.");
-                    in.next();
+                    return false;
                 }
             }
 
-            System.out.println("Would you like to submit your word or keep placing tiles? (y/n)");
+
             while(true){
+                System.out.println("Would you like to submit your word (y) or keep placing tiles?(n)");
                 try{
                     String userInput = in.next();
                     if(userInput.equalsIgnoreCase("y")) {
@@ -184,17 +189,17 @@ public class Game {
                             return true;
                         } else {
                             System.out.println("Invalid word placed");
-                            in.next();
+                            return false;
                         }
                     }else if(userInput.equalsIgnoreCase("n")){
                         break;//break loop and keep placing tiles
                     }else{
                         System.out.println("Invalid input");//user entered something other than y or n
-                        in.next();
+                        continue;
                     }
                 }catch(InputMismatchException e){
                     System.out.println("Invalid input.");
-                    in.next();
+                    continue;
                 }
             }
         }
